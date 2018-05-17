72fca8438ba37cdbb44c529fdb1524836fb1cd31
myprojectsfile <myprojectsfile@hotmail.com>
2018-05-18 01:04:25 +0430

    change docker-lib

--- server/routes/docker-lib.js
+++ server/routes/docker-lib.js
@@ -20,187 +20,73 @@ function downloadImage(image, tag, id) {
     var delete_dir_command = 'rm -r ' + dir;
     var delete_file_command = 'rm ' + dir + '.tar';
 
-    console.log('(0) ---> printing commands <-----')
-    console.log(download_command);
-    console.log(compress_command);
-    console.log(upload_command);
-    console.log(delete_dir_command);
-    console.log(delete_file_command);
-
     return new Promise(function (resolve, reject) {
-        // 1- download image layers
-        console.log('(1) ---> downloading:' + dir);
-
-        downloadImageLayer(download_command)
-            .then(() => {
-                // 2- compress image layers
-                console.log('compressing:' + dir);
-
-                compressImageLayers(compress_command)
-                    .then(() => {
-                        // 3- upload image file
-                        console.log('uploading:' + dir);
-
-                        uploadImage(upload_command)
-                            .then(() => {
-                                // 4- delete temp directory
-                                console.log('deleting directory:' + dir);
-
-                                deleteTempDir(delete_dir_command)
-                                    .then(() => {
-                                        // 5- delete temp file
-                                        console.log('deleting file:' + dir);
-
-                                        downloadImageLayer(delete_file_command)
-                                            .then(() => {
-                                                resolve(id);
-                                            })
-                                            .catch((error) => {
-                                                reject(error);
-                                            });
-                                    })
-                                    .catch((error) => {
-                                        reject(error);
-                                    });
-                            })
-                            .catch((error) => {
-                                reject(error);
-                            });
-                    })
-                    .catch((error) => {
-                        reject(error);
-                    });
-            })
-            .catch((error) => {
+        downloadImageLayer()
+            .stderr.on('data', (error) => {
                 reject(error);
+            }).on('close', (code) => {
+                if (code == 0) {
+                    compressImageLayers()
+                        .stderr.on('data', (error) => {
+                            reject(error);
+                        }).on('close', (code) => {
+                            if (code == 0) {
+                                uploadImage()
+                                    .stderr.on('data', (error) => {
+                                        reject(error);
+                                    }).on('close', (code) => {
+                                        if (code == 0) {
+                                            deleteTempDir()
+                                                .stderr.on('data', (error) => {
+                                                    reject(error);
+                                                }).on('close', (code) => {
+                                                    if (code == 0) {
+                                                        deleteTempFile()
+                                                            .stderr.on('data', (error) => {
+                                                                reject(error);
+                                                            }).on('close', (code) => {
+                                                                if (code == 0) resolve(id);
+                                                                else reject(code);
+                                                            });
+                                                    }
+                                                    else reject(code);
+                                                });
+                                        }
+                                        else reject(code);
+                                    });
+                            }
+                            else reject(code);
+                        });
+                }
+                else reject(code);
             });
     });
 }
 
 function downloadImageLayer(download_command) {
-    return new Promise((resolve, reject) => {
-        const spa = spawn(download_command);
-
-        spa.stdout.on('data', (data) => {
-        });
-
-        spa.stderr.on('data', (error) => {
-            reject(error);
-        });
-
-        spa.on('close', (code) => {
-            if (code == 0) resolve(code);
-            else reject(code);
-        });
-    })
+    const spa = spawn(download_command);
+    return spa;
 }
 
 function compressImageLayers(compress_command) {
-    return new Promise((resolve, reject) => {
-        const spa = spawn(compress_command);
-
-        spa.stdout.on('data', (data) => {
-        });
-
-        spa.stderr.on('data', (error) => {
-            reject(error);
-        });
-
-        spa.on('close', (code) => {
-            if (code == 0) resolve(code);
-            else reject(code);
-        });
-    })
+    const spa = spawn(compress_command);
+    return spa;
 }
 
 function uploadImage(upload_command) {
-    return new Promise((resolve, reject) => {
-        const spa = spawn(upload_command);
-
-        spa.stdout.on('data', (data) => {
-        });
-
-        spa.stderr.on('data', (error) => {
-            reject(error);
-        });
-
-        spa.on('close', (code) => {
-            if (code == 0) resolve(code);
-            else reject(code);
-        });
-    })
+    const spa = spawn(upload_command);
+    return spa;
 }
 
 function deleteTempDir(delete_dir_command) {
-    return new Promise((resolve, reject) => {
-        const spa = spawn(delete_dir_command);
-
-        spa.stdout.on('data', (data) => {
-        });
-
-        spa.stderr.on('data', (error) => {
-            reject(error);
-        });
-
-        spa.on('close', (code) => {
-            if (code == 0) resolve(code);
-            else reject(code);
-        });
-    })
+    const spa = spawn(delete_dir_command);
+    return spa;
 }
 
 function deleteTempFile(delete_file_command) {
-    return new Promise((resolve, reject) => {
-        const spa = spawn(delete_file_command);
-
-        spa.stdout.on('data', (data) => {
-        });
-
-        spa.stderr.on('data', (error) => {
-            reject(error);
-        });
-
-        spa.on('close', (code) => {
-            if (code == 0) resolve(code);
-            else reject(code);
-        });
-    })
+    const spa = spawn(delete_file_command);
+    return spa;
 }
-
-
-
-
-// function compressImageLayers(compress_command) {
-//     return exec(compress_command)
-//         .then(() => { console.log('(2) -----> compressing dir completed successfully') })
-//         .catch((error) => {
-//             console.log(`(2) ######## compressing error ---->:${error}`);
-//         });
-// }
-
-// function uploadImage(upload_command) {
-//     return exec(upload_command)
-//         .then(() => { console.log('(3) -----> uploading file completed successfully') })
-//         .catch((error) => {
-//             console.log(`(3) ######## uploading error ---->:${error}`);
-//         });
-// }
-
-// function deleteTempDir(delete_dir_command) {
-//     return exec(delete_dir_command)
-//         .then(() => { console.log('(4) -----> deleting dir completed successfully') })
-//         .catch((error) => {
-//             console.log(`(4) ######## deleting dir error ---->:${error}`);
-//         });
-// }
-
-// function deleteTempFile(delete_file_command) {
-//     return exec(delete_file_command)
-//         .then(() => { console.log('(5) -----> deleting file completed successfully') })
-//         .catch((error) => {
-//             console.log(`(5) ######## deleting file error ---->:${error}`);
-//         });
-// }
 
 module.exports = {
     downloadImage: downloadImage
