72c4cf5d82ff2e53c01d8efa5142400fdb2ea378
myprojectsfile <myprojectsfile@hotmail.com>
2018-05-17 15:46:35 +0430

    editing docker-lib file

--- server/routes/docker-lib.js
+++ server/routes/docker-lib.js
@@ -20,9 +20,16 @@ function downloadImage(image, tag, id) {
     var delete_dir_command = 'rm -r ' + dir;
     var delete_file_command = 'rm ' + dir + '.tar';
 
+    console.log('(0) ---> printing commands <-----')
+    console.log(download_command);
+    console.log(compress_command);
+    console.log(upload_command);
+    console.log(delete_dir_command);
+    console.log(delete_file_command);
+
     return new Promise(function (resolve, reject) {
         // 1- download image layers
-        console.log('downloading:' + dir);
+        console.log('(1) ---> downloading:' + dir);
 
         downloadImageLayer(download_command)
             .then(() => {
@@ -72,32 +79,41 @@ function downloadImage(image, tag, id) {
 
 function downloadImageLayer(download_command) {
     return exec(download_command)
-        .then()
+        .then(() => {console.log('(1) -----> downloading completed successfully')})
         .catch((error) => {
+            console.log(`(1) ######## downloading error ----> :${error}`);
         });
 }
 
 function compressImageLayers(compress_command) {
-    return exec(compress_command).then()
+    return exec(compress_command)
+        .then(() => { console.log('(2) -----> compressing dir completed successfully') })
         .catch((error) => {
+            console.log(`(2) ######## compressing error ---->:${error}`);
         });
 }
 
 function uploadImage(upload_command) {
-    return exec(upload_command).then()
+    return exec(upload_command)
+        .then(() => { console.log('(3) -----> uploading file completed successfully') })
         .catch((error) => {
+            console.log(`(3) ######## uploading error ---->:${error}`);
         });
 }
 
 function deleteTempDir(delete_dir_command) {
-    return exec(delete_dir_command).then()
+    return exec(delete_dir_command)
+        .then(() => { console.log('(4) -----> deleting dir completed successfully') })
         .catch((error) => {
+            console.log(`(4) ######## deleting dir error ---->:${error}`);
         });
 }
 
 function deleteTempFile(delete_file_command) {
-    return exec(delete_file_command).then()
+    return exec(delete_file_command)
+        .then(() => { console.log('(5) -----> deleting file completed successfully') })
         .catch((error) => {
+            console.log(`(5) ######## deleting file error ---->:${error}`);
         });
 }
 
